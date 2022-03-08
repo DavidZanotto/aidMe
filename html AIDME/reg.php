a78f1eb5c34a94d88ae9f6644709f15961ecbe6e
s01739 <s6245130c@fermibassano.edu.it>
2022-03-08 23:48:14 +0100

    testing registrazione.php

--- html AIDME/reg.php
+++ html AIDME/reg.php
@@ -4,22 +4,38 @@
 </head>
 <body>
 <?php
+	//nella tabella Utenti manca il numero di telefono dell'utente stesso
+	include("parametri.php");
 	
-	$e=$_POST["email"];
-	$n=$_POST["nome"];
+	// Connessione al server dbms
+	$connect = mysqli_connect($server, $username, $password)
+		or die("Connessione non riuscita: " . mysqli_error($connect));
+	print ("Connesso con successo <br />");
+
+	//salvataggio dei parametri
+	$nome=$_POST["nome"];
+	$email=$_POST["email"];
 	$tel=$_POST["telefono"];
-	$p=$_POST["password"];
-	echo "<h1>email passata:".$e."</h1>";
-	echo $n;
-	echo $tel;
-	echo $p;
-	/*
-	if(true){
-		//echo "<meta http-equiv=\"refresh\" target=\"_blank\" content=\"0.1;URL=aidme.tk/login.html\">";
-		//echo "<meta http-equiv=\"refresh\" target=\"_blank\" content=\"0.1;URL=aidme.tk/registrazione.html\">";
-		//echo "<h1><center><span style=\"color: #ff0000;\">ERROR</span></center></h1>";	
-	}*/
-	
+	$pass=$_POST["password"];
+
+	echo "<h1>email passata:".$nome."</h1>";	
+	echo "<h1>email passata:".$email."</h1>";	
+	echo "<h1>email passata:".$tel."</h1>";
+	echo "<h1>email passata:".$pass."</h1>";
+
+    // selezione database
+    mysqli_select_db($connect, $database)
+        or die ("Impossibile selezionare il db");
+
+    $table = "Utenti"; 
+    $query = "INSERT INTO utenti (Username, Email, password) VALUES ("
+	. "'" . $nome . "',"
+	. "'" . $email . "',"
+	. "'" . $password . "')"
+    $result = mysqli_query($connect, $query)
+        or die ("Errore nella query" . mysqli_error($connect));
+
+	mysqli_close($connect); 
 ?>
 </body>
 </html>
