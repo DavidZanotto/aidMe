21b717cf6b6c8e4f319d275b961554372016e95d
s01739 <s6245130c@fermibassano.edu.it>
2022-03-09 10:11:41 +0100

    registrazione quasi completa

--- html AIDME/reg.php
+++ html AIDME/reg.php
@@ -20,9 +20,9 @@
 /*
 	$data=$_POST["nome"];
 	$cognome=$_POST["nome"];
-
-	$data = "11settembre2001"
-	$cognome = "rossi"*/
+*/
+	$data = "2001-09-11";
+	$cognome = "rossi";
 
 	echo "<h1>email passata:".$nome."</h1>";	
 	echo "<h1>email passata:".$email."</h1>";	
@@ -36,23 +36,34 @@
     $t1 = "Utenti"; 
     $t2 = "DatiAnagrafici"; 
 
-   	$query = "INSERT INTO ".$t1." (id, Username, Email, Password, idCitta) VALUES (
-		'".rand(10,10000)."',". "'" . $nome . "',". "'" . $email . "',". "'" . $pass . "',". "'1')"; 
+	//fase per incrementare l'id al massimo+1
+	$id = "SELECT id FROM ". $t1.";";
+	$result = mysqli_query($connect, $id)
+		or die ("Errore nella query" . mysqli_error($connect));
 	
-	$result = mysqli_query($connect, $query)
+	// Visualizzo il risultato della query
+	while ($search = mysqli_fetch_array($result))
+		$i = $search[id];
+
+	$i = $i+1;
+	print $i;
+	// libero la memoria occupata dall'istruzione SELECT
+	mysqli_free_result($result);
+
+
+   	$q1 = "INSERT INTO ".$t1." (id, Username, Email, Password, idCitta) VALUES (
+		'".$i."','" . $nome . "','" . $email . "','" . $pass . "','1')"; 
+
+	$q2 = "INSERT INTO ".$t2." (id, Nome, Cognome, DataNascita, CodiceFiscale, Telefono) VALUES (
+		'".$i."','" . $nome . "','" . $cognome . "','" . $data . "','bruh','".$tel."')"; 
+		
+
+	$result = mysqli_query($connect, $q1)
 		or die ("Errore nella query" . mysqli_error($connect));
 
-/*
-	$query = "INSERT INTO ".$t2." (id, Nome, Cognome, DataNascita, CodiceFiscale, Telefono) VALUES (
-		'".rand(10,10000)."',". "'" . $nome . "',". "'" . $cognome . "',". "'" . $data . "',". "'bruh',"."'".$tel."')"; 
-	
+	$result = mysqli_query($connect, $q2)
+        or die ("Errore nella query" . mysqli_error($connect));
 
-	/*	
-$query = "INSERT INTO ".$t1." (id, Username, Email, Password, idCitta) VALUES (
-	'".rand(10,10000)."',". "'" . "dios" . "',". "'" . $email . "',". "'" . $pass . "',". "'1')"; 
-
-	$result = mysqli_query($connect, $query)
-        or die ("Errore nella query" . mysqli_error($connect));*/
 	mysqli_close($connect); 
 ?>
 </body>
