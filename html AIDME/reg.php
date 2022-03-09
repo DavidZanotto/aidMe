ac6fb2db7d23e8256bff17e029639374f3956ebb
s01739 <s6245130c@fermibassano.edu.it>
2022-03-09 01:05:05 +0100

    registrazione a buon punto circa

--- html AIDME/reg.php
+++ html AIDME/reg.php
@@ -17,6 +17,12 @@
 	$email=$_POST["email"];
 	$tel=$_POST["telefono"];
 	$pass=$_POST["password"];
+/*
+	$data=$_POST["nome"];
+	$cognome=$_POST["nome"];
+
+	$data = "11settembre2001"
+	$cognome = "rossi"*/
 
 	echo "<h1>email passata:".$nome."</h1>";	
 	echo "<h1>email passata:".$email."</h1>";	
@@ -27,14 +33,26 @@
     mysqli_select_db($connect, $database)
         or die ("Impossibile selezionare il db");
 
-    $table = "Utenti"; 
-    $query = "INSERT INTO utenti (Username, Email, password) VALUES ("
-	. "'" . $nome . "',"
-	. "'" . $email . "',"
-	. "'" . $password . "')"
-    $result = mysqli_query($connect, $query)
-        or die ("Errore nella query" . mysqli_error($connect));
+    $t1 = "Utenti"; 
+    $t2 = "DatiAnagrafici"; 
 
+   	$query = "INSERT INTO ".$t1." (id, Username, Email, Password, idCitta) VALUES (
+		'".rand(10,10000)."',". "'" . $nome . "',". "'" . $email . "',". "'" . $pass . "',". "'1')"; 
+	
+	$result = mysqli_query($connect, $query)
+		or die ("Errore nella query" . mysqli_error($connect));
+
+/*
+	$query = "INSERT INTO ".$t2." (id, Nome, Cognome, DataNascita, CodiceFiscale, Telefono) VALUES (
+		'".rand(10,10000)."',". "'" . $nome . "',". "'" . $cognome . "',". "'" . $data . "',". "'bruh',"."'".$tel."')"; 
+	
+
+	/*	
+$query = "INSERT INTO ".$t1." (id, Username, Email, Password, idCitta) VALUES (
+	'".rand(10,10000)."',". "'" . "dios" . "',". "'" . $email . "',". "'" . $pass . "',". "'1')"; 
+
+	$result = mysqli_query($connect, $query)
+        or die ("Errore nella query" . mysqli_error($connect));*/
 	mysqli_close($connect); 
 ?>
 </body>
